920871a174e6fa6b972701c9ddff9a5bd7c56762
Dean Jackson <deanishe@deanishe.net>
2018-03-31 11:58:26 +0200

    Use scselect instead of networksetup #1 #2
    - Add built-in update mechanism
    - Smarter (session-scoped) caching
    - Alfred 3-only

--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 Network Location workflow for Alfred
 ====================================
 
-View and change your network location from [Alfred 2 & 3][alfred].
+View and change your network location from [Alfred][alfred].
 
-![](https://raw.githubusercontent.com/deanishe/alfred-network-location/master/screenshot.png "")
+![][screenshot]
+
 
 ## Installation ##
 
-Grab the workflow file from the [releases page](https://github.com/deanishe/alfred-network-location/releases) or [Packal](http://www.packal.org/workflow/network-location).
+Grab the latest version from the [releases page][releases].
+
+**Note:** If you're still using Alfred 2, download [v1.0][v1.0]. Later versions are only compatible with Alfred 3.
+
 
 ## Usage ##
 
@@ -16,11 +20,25 @@ Grab the workflow file from the [releases page](https://github.com/deanishe/alfr
 	+ `↩` — Activate selected network location
 	+ `⌘+↩` — Open Network Preferences
 
-**Note:** You will probably be asked to enter your password to change the network location.
 
 ## Licence ##
 
-This workflow is released under the [MIT Licence](http://opensource.org/licenses/MIT).
+This workflow is released under the [MIT Licence][mit].
+
+
+## Changelog ##
+
+- [v2.0][v2.0] — 2018-03-31
+    - Add built-in update mechanism
+    - Use `scselect` instead of `networksetup` (no need to enter password)
+    - Smarter caching
+- [v1.0][v1.0] — 2014-06-18
+    - Initial release
 
 
 [alfred]: https://www.alfredapp.com/
+[v1.0]: https://github.com/deanishe/alfred-network-location/releases/tag/v1.0
+[v2.0]: https://github.com/deanishe/alfred-network-location/releases/tag/v2.0
+[releases]: https://github.com/deanishe/alfred-network-location/releases
+[screenshot]: https://raw.githubusercontent.com/deanishe/alfred-network-location/master/screenshot.png
+[mit]: http://opensource.org/licenses/MIT
--- src/netloc.py
+++ src/netloc.py
@@ -1,102 +1,121 @@
 #!/usr/bin/python
 # encoding: utf-8
 #
-# Copyright © 2014 deanishe@deanishe.net
+# Copyright (c) 2014 deanishe@deanishe.net
 #
 # MIT Licence. See http://opensource.org/licenses/MIT
 #
 # Created on 2014-06-17
 #
 
-"""
-List, filter and activate network locations from within Alfred
-"""
+"""List, filter and activate network locations from within Alfred."""
 
-from __future__ import print_function, unicode_literals
+from __future__ import print_function
 
-import sys
-import subprocess
 import argparse
+from collections import namedtuple
+import re
+import sys
 
-from workflow import Workflow, ICON_NETWORK, ICON_WARNING
 
-VERSION = '1.0'
+from workflow import Workflow3, ICON_NETWORK, ICON_WARNING
+from workflow.util import run_command
 
-MAX_CACHE_AGE = 10  # seconds
+HELP_URL = 'https://github.com/deanishe/alfred-network-location/issues'
+ICON_UPDATE = 'update.png'
+UPDATE_SETTINGS = {
+    'github_slug': 'deanishe/alfred-network-location'
+}
+
 
 log = None
 decode = None
+match = re.compile(r'([ *]+)([0-9A-F-]+)\s\((.+)\)').match
+
+
+Location = namedtuple('Location', 'name id current')
 
 
 def get_locations():
-    """Return list of available network locations"""
-    cmd = ['networksetup', '-listlocations']
-    output = decode(subprocess.check_output(cmd))
-    return [l.strip() for l in output.split('\n') if l.strip()]
+    """Return list of available network locations."""
+    locations = []
+
+    cmd = ['/usr/sbin/scselect']
+    output = decode(run_command(cmd))
+    for line in output.split('\n'):
+        m = match(line)
+        if not m:
+            continue
+
+        state, id_, name = [s.strip() for s in m.groups()]
+        state = True if state else False
+        loc = Location(name, id_, state)
+        log.debug('%r', loc)
+        locations.append(loc)
+
+    return locations
 
 
-def get_current_location():
-    """Return name of active network location"""
-    cmd = ['networksetup', '-getcurrentlocation']
-    return decode(subprocess.check_output(cmd)).strip()
+def do_list_locations(query=None):
+    """Display list of available locations in Alfred."""
+    if not query and wf.update_available:  # show update
+        wf.add_item('An Update is Available',
+                    u'↩ or ⇥ to install',
+                    valid=False,
+                    autocomplete='workflow:update',
+                    icon=ICON_UPDATE)
 
-
-def do_list_locations(wf, query=None):
-    """Display list of available locations in Alfred"""
-    locations = wf.cached_data('locations', get_locations,
-                               max_age=MAX_CACHE_AGE)
-
-    current_location = get_current_location()
+    locations = wf.cached_data('locations', get_locations, session=True)
 
     if query:
-        locations = wf.filter(query, locations)
+        locations = wf.filter(query, locations, key=lambda loc: loc.name)
 
-    modifier_subtitles = {'cmd': 'Open Network preferences'}
+    for loc in locations:
 
-    if not locations:
-        wf.add_item('No matching network locations',
-                    'Try a different query',
-                    icon=ICON_WARNING)
+        if loc.current:
+            title = loc.name + u' (active)'
+            subtitle = u'This location is currently active'
+            valid = False
 
-    else:
-        for loc in locations:
-            if loc == current_location:
-                # title = '✅ {}'.format(loc)
-                title = '{} (active)'.format(loc)
-                subtitle = 'This location is currently active'
-                valid = False
-            else:
-                title = loc
-                subtitle = 'Action to switch to this location'
-                valid = True
-            wf.add_item(title, subtitle, modifier_subtitles,
-                        valid=valid, arg=loc, icon=ICON_NETWORK)
+        else:
+            title = loc.name
+            subtitle = u'Switch to this location'
+            valid = True
 
+        wf.add_item(title, subtitle, valid=valid, arg=loc.id,
+                    icon=ICON_NETWORK)
+
+    wf.warn_empty('No matching network locations', 'Try a different query')
     wf.send_feedback()
 
 
-def do_set_location(wf, location):
-    """Change to location ``location``"""
-    subprocess.call(['/usr/sbin/networksetup', '-switchtolocation', location],
-                    stdout=subprocess.PIPE)
-    print(location.encode('utf-8'), end='')
+def do_set_location(name):
+    """Change to location ``name``."""
+    run_command(['/usr/sbin/scselect', name])
+    print(name.encode('utf-8'), end='')
+    wf.clear_session_cache()
 
 
 def main(wf):
+    """Run Script Filter."""
     parser = argparse.ArgumentParser()
     parser.add_argument('action', choices=('list', 'set'))
     parser.add_argument('query', nargs='?')
     args = parser.parse_args(wf.args)
 
-    log.debug('action : {!r}  query : {!r}'.format(args.action, args.query))
+    log.debug('action=%r, query=%r', args.action, args.query)
 
     if args.action == 'list':
-        return do_list_locations(wf, args.query)
+        return do_list_locations(args.query)
     elif args.action == 'set':
-        return do_set_location(wf, args.query)
+        return do_set_location(args.query)
+
 
 if __name__ == '__main__':
-    wf = Workflow()
+    wf = Workflow3(
+        help_url=HELP_URL,
+        update_settings=UPDATE_SETTINGS,
+    )
     log = wf.logger
     decode = wf.decode
     sys.exit(wf.run(main))
